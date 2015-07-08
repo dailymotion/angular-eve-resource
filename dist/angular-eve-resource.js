140c2c7c19b48adc57248da32c80f2e55dd9ca87
Rahul Doshi <rahul.doshi@dailymotion.com>
2015-07-08 08:36:08 -0700

    fix(dist): correct wrapping of IIFE as suggested by Douglas Crockford

--- dist/angular-eve-resource.js
+++ dist/angular-eve-resource.js
@@ -1,42 +1,4 @@
-!(function (angular, window, document, undefined) {
+;(function (angular, window, document, undefined) {
     'use strict';
-angular.module('com.dailymotion.ngEveResource', [
-    'ngResource'
-]);
 
-angular.module('com.dailymotion.ngEveResource')
-    .factory('eveResource', function($resource) {
-        return function(url, paramDefaults, actions, options, toJsonReplacer) {
-            var Resource,
-                toJSON;
-            
-            if (!angular.isFunction(toJsonReplacer)) {
-                if (angular.isFunction(options)) {
-                    toJsonReplacer = options;
-                    options = {};
-                } else {
-                    toJsonReplacer = function(key, value) {
-                        return value;
-                    };
-                }
-            }
-
-            Resource = $resource(url, paramDefaults, actions, options);
-
-            toJSON = Resource.prototype.toJSON;
-
-            Resource.prototype.toJSON = function() {
-                var data = toJSON.call(this);
-
-                angular.forEach(data, function(value, key) {
-                    data[key] = key.charAt(0) === '_' ? undefined : toJsonReplacer(key, value);
-                });
-
-                return data;
-            };
-
-            return Resource;
-        };
-    });
-
-})(this.angular, this, this.document);
+}(this.angular, this, this.document));
