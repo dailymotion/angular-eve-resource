d163e8fe6b6c72918f9f4a3c97d7fcd0f2a48e5b
bret.ikehara <bret.ikehara@dailymotion.com>
2016-02-05 13:45:11 -0800

    feat(helper): add a query condition helper
    add a query condition helper.

--- src/resource.factory.js
+++ src/resource.factory.js
@@ -3,7 +3,7 @@ angular.module('com.dailymotion.ngEveResource')
         return function(url, paramDefaults, actions, options, toJsonReplacer) {
             var Resource,
                 toJSON;
-            
+
             if (!angular.isFunction(toJsonReplacer)) {
                 if (angular.isFunction(options)) {
                     toJsonReplacer = options;
@@ -34,5 +34,50 @@ angular.module('com.dailymotion.ngEveResource')
             };
 
             return Resource;
+        };
+    })
+    .service('eve', function () {
+        function prepareArgs () {
+            var args = Array.prototype.slice.call(arguments);
+            return args.map(function (arg) {
+                if (angular.isFunction(arg)) {
+                    return arg();
+                }
+                return arg;
+            }).filter(function (arg) {
+                return !!arg;
+            });
+        }
+        this.query = {
+            $and: function () {
+                var cond = prepareArgs.apply(null, arguments);
+                return cond.length > 1 ? {
+                    $and: cond,
+                } : cond[0];
+            },
+            $or: function () {
+                var cond = prepareArgs.apply(null, arguments);
+                return cond.length > 1 ? {
+                    $or: cond,
+                } : cond[0];
+            },
+            $eq: function (key, val) {
+                var o;
+                if (angular.isDefined(key) && angular.isDefined(val)) {
+                    o = {};
+                    o[key] = val;
+                    return o;
+                }
+            },
+            $like: function (key, val) {
+                var o;
+                if (angular.isDefined(key) && val) {
+                    o = {};
+                    o[key] = {
+                        $regex: val
+                    };
+                    return o;
+                }
+            },
         };
     });
