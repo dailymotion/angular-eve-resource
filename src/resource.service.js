b73c45fb86cfa3a3c01ee3db57ce55c4ebe537d1
bret.ikehara <bret.ikehara@dailymotion.com>
2016-02-08 13:39:58 -0800

    refact(service): add eveResouce helper to its own service file
    add eveResouce helper to its own service file.

---
+++ src/resource.service.js
@@ -0,0 +1,46 @@
+angular.module('com.dailymotion.ngEveResource')
+    .service('eve', function () {
+        function prepareArgs () {
+            var args = Array.prototype.slice.call(arguments);
+            return args.map(function (arg) {
+                if (angular.isFunction(arg)) {
+                    return arg();
+                }
+                return arg;
+            }).filter(function (arg) {
+                return !!arg;
+            });
+        }
+        this.query = {
+            $and: function () {
+                var cond = prepareArgs.apply(null, arguments);
+                return cond.length > 1 ? {
+                    $and: cond,
+                } : cond[0];
+            },
+            $or: function () {
+                var cond = prepareArgs.apply(null, arguments);
+                return cond.length > 1 ? {
+                    $or: cond,
+                } : cond[0];
+            },
+            $eq: function (key, val) {
+                var o;
+                if (angular.isDefined(key) && angular.isDefined(val)) {
+                    o = {};
+                    o[key] = val;
+                    return o;
+                }
+            },
+            $like: function (key, val) {
+                var o;
+                if (angular.isDefined(key) && val) {
+                    o = {};
+                    o[key] = {
+                        $regex: val
+                    };
+                    return o;
+                }
+            },
+        };
+    })
