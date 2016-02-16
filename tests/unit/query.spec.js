492665855e6981832b1bab3fe10074b424753f20
bret.ikehara <bret.ikehara@dailymotion.com>
2016-02-16 11:45:09 -0800

    tests(service): split the tests into smaller units
    split the tests into smaller units.

--- tests/unit/query.spec.js
+++ tests/unit/query.spec.js
@@ -2,13 +2,17 @@ describe('eve', function () {
 
     beforeEach(module('com.dailymotion.ngEveResource'));
 
-    it('should build query', inject(function (eve) {
+    it('should build query $eq', inject(function (eve) {
         var qw = eve.query;
 
         expect(qw.$eq('asdf', undefined)).toBeUndefined();
         expect(qw.$eq('asdf', 123)).toEqual({
             'asdf': 123
         });
+    }));
+
+    it('should build query $like', inject(function (eve) {
+        var qw = eve.query;
 
         expect(qw.$like('asdf', undefined)).toBeUndefined();
         expect(qw.$like('asdf', null)).toBeUndefined();
@@ -17,6 +21,10 @@ describe('eve', function () {
                 $regex: 123
             }
         });
+    }));
+
+    it('should build query $and', inject(function (eve) {
+        var qw = eve.query;
 
         expect(qw.$and(
             undefined,
@@ -34,6 +42,26 @@ describe('eve', function () {
             ]
         });
 
+        expect(qw.$and([
+            undefined,
+            qw.$eq('asdf', 123),
+        ])).toEqual({
+            'asdf': 123
+        });
+        expect(qw.$and([
+            qw.$eq('asdf', 123),
+            qw.$eq('asdf', 123),
+        ])).toEqual({
+            $and: [
+                {'asdf': 123},
+                {'asdf': 123}
+            ]
+        });
+    }));
+
+    it('should build query $or', inject(function (eve) {
+        var qw = eve.query;
+
         expect(qw.$or(
             undefined,
             qw.$eq('asdf', 123)
@@ -44,6 +72,22 @@ describe('eve', function () {
             qw.$eq('asdf', 123),
             qw.$eq('asdf', 123)
         )).toEqual({
+            $or: [
+                {'asdf': 123},
+                {'asdf': 123}
+            ]
+        });
+
+        expect(qw.$or([
+            undefined,
+            qw.$eq('asdf', 123),
+        ])).toEqual({
+            'asdf': 123
+        });
+        expect(qw.$or([
+            qw.$eq('asdf', 123),
+            qw.$eq('asdf', 123),
+        ])).toEqual({
             $or: [
                 {'asdf': 123},
                 {'asdf': 123}
